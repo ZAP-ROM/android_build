214700f50d22764feb69c9d39c3315b3a522212e
Zeeshan <zeetherocker@gmail.com>
2015-04-22 20:58:36 +0530

    Use ZAP device tree
    Change-Id: Ic08e0a6983832d51155ae760cc937b88bbfdf4bc

--- core/product_config.mk
+++ core/product_config.mk
@@ -181,7 +181,7 @@ include $(BUILD_SYSTEM)/device.mk
 
 # A ZAP build needs only the ZAP product makefiles.
 ifneq ($(ZAP_BUILD),)
-  all_product_configs := $(shell ls device/*/$(ZAP_BUILD)/cm.mk)
+  all_product_configs := $(shell ls device/*/$(ZAP_BUILD)/zap.mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
